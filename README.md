27be973d968b34f82b79ad0e40ae4a63002e8406
Anthony Lopez-Vito <anthonylv@anothercoffee.net>
2016-02-09 23:25:31 +0000

    First working version: creates notes in Tomboy's XML format

--- README.md
+++ README.md
@@ -1,11 +1,43 @@
-# Yojimbo to Tomboy migation tool
+# Yojimbo to Tomboy migration tool
 
-A Python tool for migrating Bare Bones [Yojimbo](http://www.barebones.com/products/yojimbo/) notes to the GNOME [Tomboy](https://wiki.gnome.org/Apps/Tomboy) is a desktop note-taking application.
+A Python tool for migrating [Yojimbo](http://www.barebones.com/products/yojimbo/) notes to the GNOME [Tomboy](https://wiki.gnome.org/Apps/Tomboy) desktop note-taking application.
 
-**CAUTION:** *Consider this to be pre-alpha code.* Make a backup before running this tool. USE IS ENTIRELY AT YOUR OWN RISK.
+[More information](http://anothercoffee.net/yojimbo-to-tomboy-notes-migration-tool) about why I created the tool.
 
-First released 9 Feb 2016 by Anthony Lopez-Vito of Another Cup of Coffee Limited
-http://anothercoffee.net
+## Limitations
 
-All code is released under The MIT License.
+This current version scrapes the content from Yojimbo's [Sidekick export](http://www.barebones.com/products/yojimbo/tour-sidekick.html) and converts the pages to Tomboy's XML format. Sidekick does not export all your Yojimbo note's metadata but you will be able to save the title and note body. Time permitting, I would like to find a way to save tag data in a future version.
+
+
+## Requirements, setup and usage
+
+* Python 2.7
+* pip install -r requirements.txt
+
+1. Grab the script files and install the required modules
+1. [Follow the instructions](http://www.barebones.com/products/yojimbo/tour-sidekick.html) to export your Sidekick pages from Yojimbo. Make sure the export worked and you can browse the pages through your web browser.
+1. Along with the Yojimbo2Tomboy script files, you'll fine a `settings-template.yml`. Rename it to `settings.yml`.
+1. In the `settings.yml` file, set the `crawler:root_dir:` setting to your Yojimbo Sidekick `contents` directory. For example, if you created Sidekick in `/Users/username/Sites/Yojimbo`, you'll need to set it to `/Users/username/Sites/Yojimbo/contents`. You might want to copy the `contents` folder to a temporary location and run the crawler there.
+1. In the `settings.yml` file, set the `writer:export_dir:` setting to the directory for the Tomboy notes files.
+
+### Usage
+
+Run:
+
+`$ python yojimbo2tomboy.py`
+
+### Copying to Tomboy
+
+1. Quit Tomboy if it's running.
+1. Copy the files in your export directory to Tomboy's notes directory. For example, in Linux mint you'll find it in `/home/username/.local/share/tomboy
+1. Restart Tomboy and you should see the Yojimbo notes organised in the *YojimboSidekick* notebook.
+
+Thi script uses the same basename and the Yojimbo Sidekick HTML files. Tomboy uses a different naming convention so it's unlikely you'll encounter problems with overwriting any existing Tomboy notes.
+
+
+## Reminder
+Remember to backup your Tomboy notes and Yojimbo Sidekick content before running this tool.
+
+## License
+Written by Anthony Lopez-Vito of [Another Cup of Coffee Limited](http://anothercoffee.net). All code is released under The MIT License.
 Please see LICENSE.txt.
--- yojimbo2tomboy.py
+++ yojimbo2tomboy.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Walk a directory structure to find files
+"""Migrate Bare Bones Yojimbo Notes to Tomboy Note format.
 
-Skeleton code to walk through a directory structure to find files.
+Usage: yojimbo2tomboy.py
+
 """
 import sys, getopt, os, time
 import os.path
@@ -11,6 +12,7 @@ import logging, logging.handlers
 from datetime import datetime
 from bs4 import BeautifulSoup
 import yaml
+from jinja2 import Environment, FileSystemLoader
 
 logger = logging.getLogger()
 
@@ -96,33 +98,38 @@ def create_directory(path):
             if not path:
                 logging.error("The path string is empty")
     else:
-        logging.error("The directory %s already exists", path)
+        logging.info("The directory %s already exists", path)
   
 
-def crawl_yojimbo_sidekick(crawler_settings):
+def crawl_yojimbo_sidekick(source_dir, export_dir):
     """Crawl the Yojimbo Sidekick export directory.
+    
+    Crawl the Yojimbo Sidekick export directory looking for
+    only .html files and ignore dot files. Write in Tomboy note format.
     """
-    root_dir = crawler_settings['root_dir']
-    #Use generator to walk through all subdirectories
-    for subdirectories, directories, files in os.walk(root_dir):         
-        for fname in files:
-            if fname.endswith('.html') and not fname.startswith('.'):
-                logging.debug("Parsing %s", fname)
-                filepath = os.path.join(subdirectories, fname)
-                title, body = get_yojimbo_note(filepath)
-                
-                #print title
-                #print body
-            else:
-                logging.warning("File skipped: %s", fname ) 
+    accepted = 0
+    skipped = 0
+    if os.path.exists(source_dir):
+        for subdirectories, directories, files in os.walk(source_dir):
+            for fname in files:
+                if fname.endswith('.html') and not fname.startswith('.'):
+                    logging.debug("Parsing %s", fname)
+                    filepath = os.path.join(subdirectories, fname)
+                    title, body = get_yojimbo_note(filepath)
+                    notefname = os.path.splitext(fname)[0]+'.note'               
+                    write_tomboy_note(title, body, export_dir, notefname)
+                    accepted += 1
+                else:
+                    skipped += 1
+                    logging.warning("File skipped: %s", fname )
+    else:
+        logging.error("Can't find source directory: %s", root_dir)
+    logging.info("Files processed: %s", accepted)
+    logging.info("Files skipped: %s", skipped)
 
 
 def get_yojimbo_note(filename):
     """Get content from a Yojimbo Sidekick note.
-    
-    There are two types of notes marked by the body class:
-    * note
-    * web_archive
     """
     title = None
     body = None
@@ -140,6 +147,10 @@ def get_yojimbo_note(filename):
 
 def parse_yojimbo_note(note_content):
     """Parse the content of a Yojimbo note file.
+    
+    There are two types of notes marked by the body class:
+    * note
+    * web_archive
     """
     content = BeautifulSoup(note_content, 'html.parser')
     title = None
@@ -169,7 +180,9 @@ def parse_yojimbo_note(note_content):
     return title, body
 
 
-def write_tomboy_note(title, body):
+def write_tomboy_note(title, body, export_dir, filename):
+    """Write a Tomboy note in XML format to export directory.
+    """
     iso_time = datetime.utcnow().isoformat()
     last_change_date = iso_time
     last_metadata_change_date = iso_time
@@ -182,13 +195,46 @@ def write_tomboy_note(title, body):
         loader=FileSystemLoader(os.path.join(template_path, 'templates')),
         trim_blocks=False)
 
-    fname = "output/index.html"
+    output_path = os.path.join(export_dir, filename)
     context = {
-        'content_dict': content_dict,
-        'content_body': content_body
+        'title': title,
+        'body': body,
+        'last_change_date': last_change_date,
+        'last_metadata_change_date': last_metadata_change_date,
+        'create_date': create_date
     }
-    with open(fname, 'w') as f:
-        html = env.get_template('index.html').render(context)
+    logging.debug("Creating note at %s", output_path)
+
+    with open(output_path, 'w') as f:
+        html = env.get_template('tomboy-note.xml').render(context)
+        f.write(html.encode('utf8'))
+
+
+def write_tomboy_notebook_template(export_dir):
+    """Write a Tomboy note in XML format to export directory.
+    """
+    iso_time = datetime.utcnow().isoformat()
+    last_change_date = iso_time
+    last_metadata_change_date = iso_time
+    create_date = iso_time
+
+    template_path = os.path.dirname(os.path.abspath(__file__))    
+
+    env = Environment(
+        autoescape=False,
+        loader=FileSystemLoader(os.path.join(template_path, 'templates')),
+        trim_blocks=False)
+
+    output_path = os.path.join(export_dir, 'tomboy-notebook-template.note')
+    context = {
+        'last_change_date': last_change_date,
+        'last_metadata_change_date': last_metadata_change_date,
+        'create_date': create_date
+    }
+    logging.debug("Creating note at %s", output_path)
+
+    with open(output_path, 'w') as f:
+        html = env.get_template('tomboy-notebook.xml').render(context)
         f.write(html.encode('utf8'))
 
 
@@ -202,13 +248,18 @@ def main(argv):
     setup_logging(settings['logger'])
     logging.info("Starting")
     
-    crawler_settings = settings['crawler']
-    crawl_yojimbo_sidekick(crawler_settings)
-    #get_yojimbo_note('source.html')
-                    
-    #export_dir = settings['writer']['export_dir']
-    #create_directory(export_dir)
-    
+    export_dir = settings['writer']['export_dir']
+    create_directory(export_dir)
+    write_tomboy_notebook_template(export_dir)
+
+    crawler_root_dir = settings['crawler']['root_dir']
+    crawl_yojimbo_sidekick(crawler_root_dir, export_dir)
+    """
+    # Test one item
+    title, body = get_yojimbo_note('sample-source.html')
+    write_tomboy_note(title, body, export_dir, 'sample.xml')
+    """
+    logging.info("Export complete")
     exit(0)
 
 
